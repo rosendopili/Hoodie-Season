3118bd1f5bf68b68db83e6f1dd31941841bb4f06
rosendopili <rosendopili@gmail.com>
2019-11-06 19:17:03 -0500

    deletions of commented out code

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-console.log('main.js is connected!');
-
-/*
-
-Here's an overview of the steps you'll follow to get your app to work...
-
-
-
-STEPS
-
-1. when the page loads
-  - add an event listener to the button
-2. When the button is clicked
-  - grab the input
-  - store the value
-  - make an API request based on the input value
-3. When the API response is returned
-  - grab all the appropriate DOM elements
-  - append the data to the DOM
-
-*/
-
-
 const getWeather = function(event) {
   document.getElementById('placeHolder').style.display = "none";
   event.preventDefault();
@@ -56,11 +33,3 @@ const getWeather = function(event) {
     };
 
 document.getElementById('submit').addEventListener('click', getWeather);
-
-
-//color change for extreme temperatures
-// if (currentTemp > 40){
-//   then (document.getElementById('temp').style.color('blue'))
-// }else if (currentTemp < 90){
-//   then (document.getElementById('temp').style.color('red'))
-// };
